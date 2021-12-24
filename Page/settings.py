500eb6d72eeac1a87d99a9c255e139fcecb96b06
sawnaysoe.mm@gmail.com <sawnaysoe.mm@gmail.com>
2021-12-24 19:16:11 +0630

    modified:   Page/settings.py

--- Page/settings.py
+++ Page/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
 # Default primary key field type
